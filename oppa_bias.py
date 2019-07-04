c5a4842b6e0b09f0ff35ba4e284e9225a13f6cbb
Ozge Yoluk <ozgeyoluk@proteinart.net>
2019-07-04 13:54:17 -0400

    Srt parsing

--- oppa_bias.py
+++ oppa_bias.py
@@ -5,9 +5,15 @@ import requests
 import lxml.html as lh
 import pandas as pd
 from bs4 import BeautifulSoup as bs
-import load json
+import json
+from selenium import webdriver
 
-namechanges={'Another Miss Oh':'Another Oh Hae Young','Birth of a Beauty':'Birth of the Beauty','Goblin':'Guardian: The Lonely and Great God'}
+namechanges={'Another Miss Oh':'Another Oh Hae Young','Birth of a Beauty':'Birth of the Beauty','Goblin':'Guardian: The Lonely and Great God','Fight For My Way':'Fight My Way',
+			'Goong':'Princess Hours',"It’s Okay, That’s Love":"It’s Okay, That’s Love",'Item':'The Item','Jealousy Incarnate':"Don't Dare to Dream (Jealousy Incarnate)",
+			'Kill Me, Heal Me':'Kill Me Heal Me',"My Wife’s Having an Affair this Week":'My Wife Is Having an Affair This Week','Remember – War of the Son':'Remember',
+			'Romantic Doctor, Teacher Kim':'Romantic Doctor Kim','Sassy Go Go':'Cheer Up!','The Great Seducer':'Tempted','The Guardians':'The Guardian','The Heirs':'Heirs',
+			"The Master's Sun":"Master's Sun","What's Wrong With Secretary Kim":"What’s Wrong With Secretary Kim","You're All Surrounded":"You're All Surrounded",
+			'Advertising Genius Lee Tae Baek':'AD Genius Lee Tae Baek','Blade Man':'Iron Man','Flower Boy Ramen Shop':'Flower Boy Ramyun Shop','Goong S':'Prince Hours'}
 
 def parselist(httppath):
 	'''Parses the list from mydrama list site
@@ -95,7 +101,7 @@ def dict_append_on_duplicates(ordered_pairs):
 			d[k] = v
 	return d
 
-def parsevideovikiurl(url):
+def parsevideovikiurl_requestway(url):
 	'''Obtains the video urls from Viki page of the drama. Does not care about the order of the episodes in the returned list.
 	Issues: Altough video list exists, it is not parsed when there is a restriction based on location, or decoding errors are raised when text is sent with /n characters.'''
 	completeset=True
@@ -112,7 +118,7 @@ def parsevideovikiurl(url):
 	try:
 		js['episode']
 	except KeyError:
-		print(soup.find_all("script").text)
+		return(soup.find_all("script"))
 		episodes=[]
 	else:
 		for epi in js['episode']:
@@ -121,12 +127,54 @@ def parsevideovikiurl(url):
 			completeset=False
 	return(completeset,episodes)
 
+
+def parsevideovikiurl_selenium():
+	'''Obtains the video urls from Viki page of the drama. Does not care about the order of the episodes in the returned list.
+	Issues: Altough video list exists, it is not parsed due to login requirements'''
+	soup = bs(driver.page_source)
+	try:
+		js = json.loads(soup.find("script",type="application/ld+json").text,object_pairs_hook=dict_append_on_duplicates)
+	except json.decoder.JSONDecodeError:
+		episodes=None
+	episodes=[]
+	try:
+		js['episode']
+	except:
+		episodes=None
+	else:
+		for epi in js['episode']:
+			episodes.append(epi['publication'][0]['publishedOn']['url'])
+	return(episodes)
+
+
 def cleantitle(title):
 	'''Cleans the title name for an easier pandas access.
-	Issues: It currently does not remove special characters'''
+	Issues: It currently does not remove special characters
+	NotUsed: Returning a dictionary with title names not needed anymore'''
 	return title.strip().lower().replace(' ', '_').replace('(', '').replace(')', '')
 
-def getvikivideolist(cdf):
+
+def openvikiselenium():
+	'''This function opens up the chrome driver for logging in. This needs to be run once only. Using requests not all javascripts are available.'''
+	browser = webdriver.Chrome()
+	browser.get("https://www.viki.com")
+	return(browser)
+
+def newtab(driver,url):
+	'''Opens a new tab and switches the focus to the second one.
+	Issues: It assumes there is only one other tab open'''
+	driver.execute_script("window.open('%s')" %url)
+	driver.switch_to.window(driver.window_handles[1])
+
+def closetab(driver):
+	'''Closes the second tab and switches to the first one'''
+	handles=driver.window_handles
+	driver.switch_to.window(handles[1])
+	driver.close()
+	driver.switch_to.window(handles[0])
+
+
+def getvikivideolist(cdf,driver):
 	'''Expands the initial list of dramas with episode url. Titles are turned to lower case and cleanded'''
 	videourls={}
 	titles=cdf['Title']
@@ -135,13 +183,40 @@ def getvikivideolist(cdf):
 		if not url:
 			print('Drama "%s" not found on Viki' %title)
 			continue
-		complete,urls=parsevideovikiurl(url)
-		if not complete and len(urls) != 0:
-			print('Warning: Total number of episodes for %s do not match the extracted url. Returning the parsed data anyway.' %title)
-		elif len(urls) == 0:
-			print('No video url found for drama %s' %title)
-			continue
-		else:
-			videourls[cleantitle(title)]=urls
-	videourls=pd.DataFrame(videourls)
+		newtab(driver,url)
+		urls=parsevideovikiurl_selenium()
+		closetab(driver)
+		if urls:
+			videourls[title]=urls
 	return(videourls)
+
+def getsublink(url):
+	'''Downloads the Korean subtitles for the url. Returns the percentage number associated with the language as well.'''
+	dbase="http://downsub.com/?url=%s"
+	d="http://downsub.com/%s"
+	sub=requests.get(dbase %url)
+	soup=bs(sub.text)
+	#The texts are only download. To find which one in the list the Korean we have to search the soup again
+	sublinks=[link.get('href')[2:] for link in bssub.find('div',id="show").find_all('a')]
+	ind=0
+	for lang in bssub.find('div',id="show").text.split('\n')[1].split('>>')[1:]:
+		if lang.split()[1].strip()=='Korean':
+			break
+		ind+=1
+	return(sublinks[ind])
+
+def savesub(url,title,episode):
+	'''It saves an srt file from the returned url of getsublink.
+	Issues: Redirects are allowed, and I have no idea what kind of security problems this would create.
+	'''
+	outname='%s_%d.srt' %(title,episode)
+	srt=requests.get(url,allow_redirects=True)
+	passed=0
+	#Some checks wheter what we have is correct
+	if srt.headers.get('content-disposition') == 'attachment; filename="[DownSub.com] .srt"':
+		passed+=1
+	if srt.headers.get('content-type') == 'application/x-subrip':
+		passed+=2
+	if passed == 2:
+		open(outname, 'w').write(srt.content)
+
